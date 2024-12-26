9c9e000cd2bccd7faff626ef63bb2e2f3723320c
MocusEZ <mocusz@163.com>
2024-12-26 19:32:06 +0800

    feat: add CLI commands for creating new posts and syncing site

--- README.md
+++ README.md
@@ -34,4 +34,6 @@ All commands are run from the root of the project, from a terminal:
 | `npm run preview`         | Preview your build locally, before deploying     |
 | `npm run astro ...`       | Run CLI commands like `astro add`, `astro check` |
 | `npm run astro -- --help` | Get help using the Astro CLI                     |
+| `npm run newpage`         | Generate new post base on template               |
+| `npm run sync`            | Publish the site by system script                |
 
---
+++ cli.js
@@ -0,0 +1,73 @@
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+import { randomBytes } from 'crypto';
+import { fileURLToPath } from 'url';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execPromise = promisify(exec);
+
+const generatePost = async (title = 'My Post', category = '日常笔记') => {
+  try {
+    const now = new Date();
+    const dateStr = now.toLocaleString('zh-CN', {
+      year: 'numeric',
+      month: '2-digit', 
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    }).replace(/\//g, '-');
+
+    const abbrlink = randomBytes(2).toString('hex');
+    const template = `---
+title: ${title}
+sticky: 0
+cover: 
+categories: ${category}
+abbrlink: ${abbrlink}
+description: 
+date: ${dateStr}
+updated: ${dateStr}
+tags:
+---
+
+Write your content here...
+`;
+    const sanitizedTitle = title.toLowerCase().replace(/\s+/g, '-');
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const filePath = join(__dirname, 'src/content/blog', `${sanitizedTitle}.md`);
+
+    await writeFile(filePath, template, 'utf8');
+    console.log(`✨ Created new post: ${filePath}`);
+  } catch (error) {
+    console.error('Failed to create post:', error);
+  }
+};
+
+
+const updateSite = async () => {
+  try {
+      const command = 'echo Hello'
+      const { stdout, stderr } = await execPromise(command);
+      if (stdout) console.log(stdout);
+      if (stderr) console.error(stderr);
+  } catch (error) {
+      console.error('Failed to update:', error);
+  }
+};
+
+const command = process.argv[2];
+const args = process.argv.slice(3);
+
+switch (command) {
+    case 'newpage':
+        generatePost(...args);
+        break;
+    case 'sync':
+        updateSite();
+        break;
+    default:
+        console.log('Usage:\n pnpm newpage <title> [category] \n pnpm sync');
+}
