473b3f5c2d778bb36aed549694276ef1b9a9d99e
MocusEZ <mocusz@163.com>
2024-12-26 17:48:15 +0800

    feat: integrate Waline comments system

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,14 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://mocusez.site',
   integrations: [sitemap()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // Add NPM package CSS/SCSS here for global import
+          additionalData: `@import "@waline/client/style";`
+        }
+      }
+    }
+  }
 });
